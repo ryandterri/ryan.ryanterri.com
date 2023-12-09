8284a7b0e8aec611e2c91ffde41f2f705d427a35
Ryan Purcell <patrick.ryan.purcell@gmail.com>
2023-12-09 00:22:16 -0600

    first commit

---
+++ src/components/SideBar.vue
@@ -0,0 +1,51 @@
+<script setup>
+
+import TailwindIcon from "../components/icons/TailwindIcon.vue";
+import NodeJsIcon from "../components/icons/NodeJsIcon.vue";
+import VueJsIcon from "../components/icons/VueJsIcon.vue";
+import MongoDbIcon from "../components/icons/MongoDbIcon.vue";
+</script>
+
+<template>
+  <aside class="fixed top-0 left-0 h-screen w-64">
+    <div class="h-full px-3 py-4 overflow-y-auto bg-gray-700 text-white">
+      <div class="flex flex-col items-center justify-center">
+        <img src="../assets/profile.jpg" alt="profile" class="rounded-full h-28 w-28"/>
+        <div class="font-bold">Patrick Ryan Purcell</div>
+        <div class="">Software Developer / Architect</div>
+      </div>
+      <hr class="mb-4 mt-4">
+      <div class="font-bold text-xl ml-2 mb-4">Skills</div>
+      <ul class="space-y-2 font-medium">
+        <li>
+          <a class="flex items-center p-2 rounded-lg gap-4">
+            <vue-js-icon class="h-6 w-6"></vue-js-icon>
+            <span>Vue.js</span>
+          </a>
+        </li>
+        <li>
+          <a class="flex items-center p-2 rounded-lg gap-4">
+            <node-js-icon class="h-6 w-6"></node-js-icon>
+            <span>Node.js</span>
+          </a>
+        </li>
+        <li>
+          <a class="flex items-center p-2 rounded-lg gap-4">
+            <tailwind-icon class="h-6 w-6"></tailwind-icon>
+            <span>Tailwind CSS</span>
+          </a>
+        </li>
+        <li>
+          <a class="flex items-center p-2 rounded-lg gap-4">
+            <mongo-db-icon class="h-6 w-6"></mongo-db-icon>
+            <span>MongoDb</span>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </aside>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/composables/profile.js
@@ -0,0 +1,80 @@
+export const contact_links = [
+    { icon: 'mdi-phone', value: '281-733-9242', link: 'tel:281-733-9242' },
+    {
+        icon: 'mdi-email',
+        value: 'patrick.ryan.purcell@gmail.com',
+        link: 'mailto:patrick.ryan.purcell@gmail.com'
+    },
+    {
+        icon: 'mdi-linkedin',
+        value: 'https://www.linkedin.com/in/ryan-purcell-6ab1368/ ',
+        link: 'https://www.linkedin.com/in/ryan-purcell-6ab1368/ ',
+        target: '_blank'
+    },
+    {
+        icon: 'mdi-github',
+        value: 'https://github.com/ryandterri',
+        link: 'https://github.com/ryandterri',
+        target: '_blank'
+    },
+    {
+        icon: 'mdi-web',
+        value: 'https://resume.ryanterri.com',
+        link: 'https://resume.ryanterri.com',
+        target: '_blank'
+    },
+]
+export const profile = {
+    name: 'Patrick Ryan Purcell',
+    headline: 'Dedicated to designing simple, elegant and effective solutions',
+    description: 'I happen to be lucky enough to do what I love to do. Designing and creating helpful solutions to problems is not "work" to me but something I enjoy.',
+}
+export const work_history = [
+    {
+        company: 'Compare Power',
+        title: 'IT Architect',
+        dates: 'February 2021 - Present',
+        description: 'Creating full stack solutions for both customer facing and internal systems.  Using primarily nodejs, MongoDb and VueJs'
+    },
+    {
+        company: 'Stratum Reservoir',
+        title: 'IT Architect',
+        dates: 'January 2020 - February 2021',
+        description: 'Developed an api and frontend for serving analytical data for clients. Built with nodejs and mongodb and secured with jwt tokens.'
+    },
+    {
+        company: 'Gexa Energy',
+        title: 'IT Architect',
+        dates: 'August 2016 - January 2020',
+        description: 'Developing an api for 3rd party brokers to send us new client sign ups. One version in C# Mvc with Oracle and one using Node.js with mongodb. Secured with jwt tokens.'
+    },
+    {
+        company: 'Weatherford Labs',
+        title: 'IT Technical Architect',
+        dates: 'January 2015 - August 2016',
+        description: 'Created a web api for the internal LIMS system using C# and Sql Server. Also created a new frontend for the LIMS system using .Net Mvc and angularjs.'
+    },
+    {
+        company: 'Gexa Energy',
+        title: 'Tech Lead',
+        dates: 'January 2012 - January 2015',
+        description: 'Managed outcomes and expectations for projects assigned to my team. Team size consisted of 4-8 developers. Also created a new ui and backend for our commercial portal.'
+    },
+    {
+        company: 'Weatherford Labs',
+        title: 'Software Developer',
+        dates: 'November 2008 - January 2012',
+        description: 'Created / Upgraded our project management system from classic asp to .Net Mvc.'
+    },
+]
+export const interests = [
+    { title: 'Tesla', description: 'Got mine in March of 2020 and I love it.' },
+    {
+        title: 'CI/CD',
+        description: 'The simplicity of being able to check in code and have it build, test and deploy is amazing.'
+    },
+    {
+        title: 'Homemade Salsa',
+        description: 'Fresh ingredients. Great seasoning. Crisp chips. What else do you need?'
+    },
+]
