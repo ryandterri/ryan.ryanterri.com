a505771fad5eebbd9dcd4ef7ff0a1cb069ecaba0
Ryan Purcell <patrick.ryan.purcell@gmail.com>
2023-12-09 00:24:18 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { profile, work_history, interests, contact_links } from "./composables/profile.js";
+import { UserIcon, ClockIcon, ChatBubbleBottomCenterTextIcon, LightBulbIcon } from '@heroicons/vue/24/solid'
+import SideBar from "./components/SideBar.vue";
+</script>
+
+<template>
+  <side-bar></side-bar>
+
+  <div class="p-4 sm:ml-64 bg-white text-black h-screen">
+    <div>{{profile.name}}</div>
+    <div>{{profile.headline}}</div>
+    <div>{{profile.description}}</div>
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
